329b3420e6bfcf07b7a1fbc4d29058d55c632dbd
Divyanshu Saxena <dssaxena2011@gmail.com>
2018-09-30 23:05:15 +0530

    Removed data messages from console log

--- gobackn.py
+++ gobackn.py
@@ -20,7 +20,7 @@ def from_network_layer():
 def to_network_layer(msg):
     # Todo -> Write this message onto a file
     print ('--------------------------------')
-    print ('Received message: {0}\n'.format(msg))
+    print ('Received message.')
 
 def between(a, b, c):
     abc = a<=b and b<c
@@ -34,21 +34,23 @@ def parse_message(msg):
     # Parse the message and return a dictionary of seq_num, info and ack. Done.
     r = {}
     r['seq'] = int(msg[0:32], 2)
-    r['info'] = msg[256:-32]
     r['ack'] = int(msg[32:64], 2)
+    r['info'] = msg[256:-32]
     return r
 
 def send_data(frame_nr, frame_expected, buffer):
     global ADDR
     sinfo = buffer[frame_nr]
     sseq = "{0:b}".format(frame_nr)
-    sack = "{0:b}".format((frame_expected + MAX_SEQ) % (MAX_SEQ + 1))
+    # Ack of the received frame
+    ack = (frame_expected + MAX_SEQ) % (MAX_SEQ + 1)
+    sack = "{0:b}".format(ack)
     # Construct the string to be sent. Done.
     # Todo -> Add check sum error check
     msg = add_zeroes(sseq, 32) + add_zeroes(sack, 32) + add_zeroes('', 196) + sinfo
     SOCKET.sendall(msg.encode('utf-8'))
     print ('--------------------------------')
-    print ('Sent the message: {0}\n'.format(msg))
+    print ('Sent the message with: frame:{0}\tack:{1}\n'.format(frame_nr, ack))
     # Start the timer thread
 
 def gobackn(socket, start_first):
@@ -74,23 +76,23 @@ def gobackn(socket, start_first):
             frame_arrival = True
         else:
             start_first = False
-            
-        if NETWORK_LAYER_READY:
-            buffer.append(from_network_layer())
-            nbuffered = nbuffered + 1
-            send_data(next_frame_to_send, frame_expected, buffer)
-            next_frame_to_send = next_frame_to_send + 1
 
         if frame_arrival:
             r = parse_message(msg)
             if r['seq'] is frame_expected:
-                print ('Received expected frame')
+                print ('Received expected frame, with ack: {0}'.format(r['ack']))
                 to_network_layer(r['info'])
                 frame_expected = frame_expected + 1
             while between(ack_expected, r['ack'], next_frame_to_send):
                 nbuffered = nbuffered - 1
                 # Todo -> Stop the timer thread
                 ack_expected = ack_expected + 1
+            
+        if NETWORK_LAYER_READY:
+            buffer.append(from_network_layer())
+            nbuffered = nbuffered + 1
+            send_data(next_frame_to_send, frame_expected, buffer)
+            next_frame_to_send = next_frame_to_send + 1 
 
         timeout = False     # Todo -> Make a timer thread and check if timer has expired
         if timeout:
